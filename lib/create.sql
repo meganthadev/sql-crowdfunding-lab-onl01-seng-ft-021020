e1b485dd6f6279af1ed6ed8cee912e97162c54c8
Megan Horsfield <meganthadev@gmail.com>
2020-03-17 05:34:39 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -20,3 +20,23 @@ user_id INTEGER,
 project_id INTEGER
 );
 
+CREATE TABLE projects (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal NUMERIC,
+  start_date TEXT,
+  end_date TEXT
+);
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
+);
+
+CREATE TABLE pledges (
+  id INTEGER PRIMARY KEY,
+  amount NUMERIC,
+  project_id INTEGER,
+  user_id INTEGER
